fc7495d3d4232019059e10af0b9189e336b3ccc1
Nguyen Thi Quy <85114929+QuyNguyen1006@users.noreply.github.com>
2021-07-11 22:53:40 +0700

    Update critial_atomic_ordered.cpp

--- critial_atomic_ordered.cpp
+++ critial_atomic_ordered.cpp
@@ -19,29 +19,29 @@ int main()
             printf("odd number : %d \n", id);
     }
 
-    #pragma omp parallel num_threads(4) shared(x, sum) private(id, sumlocal)
+         #pragma omp parallel num_threads(4) shared(x, sum) private(id, sumlocal)
     {
         id = omp_get_thread_num();
         sumlocal = 0;
-    #pragma omp for
+         #pragma omp for
         for (i = 0; i < 5; i++)
             sumlocal += x[i]*2;
-    #pragma omp critical (calulate_sum)
+         #pragma omp critical (calulate_sum)
         {
             sum += sumlocal;
             printf("id = %d: sumlocal= %d sum = %d\n", id, sumlocal, sum);
         }
     }
     printf("After parallel: sum = %d \n", sum);
-#pragma omp parallel  for private(i) ordered
+    #pragma omp parallel  for private(i) ordered
     for (i = 0; i < 20; i++) {
-#pragma omp ordered
+    #pragma omp ordered
         printf("\nthread[%d]: hello world", omp_get_thread_num());
     }
     sum = 0;
-#pragma omp parallel for private(i)
+    #pragma omp parallel for private(i)
     for (i = 0; i < 5; i++) {
-#pragma omp atomic
+    #pragma omp atomic
         sum += i * 3;
     }
     printf("\n summmm = %d", sum);
